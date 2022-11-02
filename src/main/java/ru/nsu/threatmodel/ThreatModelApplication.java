2be7e593d3ee8bd5cd39d1435805307857c44c11
Evgeniy Bokov <zheka2001941@gmail.com>
2022-11-02 20:04:20 +0700

    Initial commit

---
+++ src/main/java/ru/nsu/threatmodel/ThreatModelApplication.java
@@ -0,0 +1,13 @@
+package ru.nsu.threatmodel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThreatModelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThreatModelApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/nsu/threatmodel/ThreatModelApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.nsu.threatmodel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThreatModelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
