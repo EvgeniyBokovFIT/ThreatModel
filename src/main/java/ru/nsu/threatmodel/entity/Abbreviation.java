ad2365bddff088399dda0d2b79b4fb102537903b
Evgeniy Bokov <zheka2001941@gmail.com>
2022-12-18 17:51:33 +0700

    init

---
+++ src/main/java/ru/nsu/threatmodel/entity/Abbreviation.java
@@ -0,0 +1,14 @@
+package ru.nsu.threatmodel.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+
+@Entity
+@AllArgsConstructor
+@Getter
+@Setter
+public class Abbreviation {
+}
